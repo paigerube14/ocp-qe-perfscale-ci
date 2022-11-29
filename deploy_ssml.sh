34a9ecfe5df03b02886d9fcc431f1f04411c116d
Paige Rubendall <prubenda@redhat.com>
2022-11-28 21:42:00 -0500

    adding opeartor results

--- deploy_ssml.sh
+++ deploy_ssml.sh
@@ -10,9 +10,10 @@ ls dast_tool
 console_url=$( oc get routes console -n openshift-console -o jsonpath='{.spec.host}')
 
 token=$(oc whoami -t)
-echo $token
 
 curl -k "https://${console_url}/api/kubernetes/openapi/v2" -H "Cookie: openshift-session-token=${token}"  -H "Accept: application/json"  >> openapi.json
+
+ls -la
 
 kubectl apply -f operator_configs/catalog_source.yaml
 kubectl apply -f operator_configs/subscription.yaml
@@ -23,4 +24,4 @@ kubectl apply -f operator_configs/operatorgroup.yaml
 kubectl apply -f dast_tool/operator/config/samples/research_v1alpha1_rapidast.yaml
 
 mkdir results
-bash results.sh rapidast-pvc results
+bash dast_tool/operator/results.sh rapidast-pvc results
