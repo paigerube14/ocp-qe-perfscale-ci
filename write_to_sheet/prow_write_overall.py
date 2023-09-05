0a422b9fed0f7415926ff21a890f99b39603c9c1
Paige Rubendall <prubenda@redhat.com>
2023-09-05 09:13:42 -0400

    prow env

--- write_to_sheet/prow_write_overall.py
+++ write_to_sheet/prow_write_overall.py
@@ -18,6 +18,8 @@ data_source = "QE%20kube-burner"
 uuid = ""
 
 
+# vars here: https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/origin-ci-test/pr-logs/pull/openshift_release/42837/rehearse-42837-periodic-ci-openshift-qe-ocp-qe-perfscale-ci-main-aws-4.14-ocp-qe-perfscale-aws-ci-tests-write/1697269049693573120/artifacts/ocp-qe-perfscale-aws-ci-tests-write/openshift-qe-write-perfscale-results-all/build-log.txt
+
 def write_prow_results_to_sheet():
     scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -30,20 +32,30 @@ def write_prow_results_to_sheet():
     #open sheet
     index = 2
 
-    es_username = os.getenv("ES_USERNAME")
-    es_password = os.getenv("ES_PASSWORD")
+    # es_username = os.getenv("ES_USERNAME")
+    # es_password = os.getenv("ES_PASSWORD")
 
     task_id=os.getenv("BUILD_ID")
     job_id=os.getenv("JOB_NAME")
+    cluster_type = os.getenv("CLUSTER_TYPE")
+
     prow_base_url = "https://prow.ci.openshift.org/view/gs/origin-ci-test/logs"
     build_url=prow_base_url + "/" + job_id+ "/"+ task_id
     #open sheet
+
+    cluster_profile_dir = os.getenv("CLUSTER_PROFILE_DIR")
+    with open(cluster_profile_dir, r) as r: 
+        profile_str = r.read()
+    print('profile str ' + str(profile_str))
+    
+
+    find_version = "4." + job_id.split("4.")[-1].split("-")[0]
     index = 2
     job_url_cell = f'=HYPERLINK("{build_url}","PROW")'
     tz = timezone('EST')
     row = [job_url_cell, str(datetime.now(tz))]
     
-    ws = sheet.worksheet("4.14")
+    ws = sheet.worksheet(find_version)
     ws.insert_row(row, index, "USER_ENTERED")
 
 write_prow_results_to_sheet()
